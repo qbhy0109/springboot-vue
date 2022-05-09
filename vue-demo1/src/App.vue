19f2243bda1cbde7ddf61a594acaac74d9a38989
yb <3100954150@qq.com>
2022-05-09 09:08:31 +0800

    1

---
+++ vue-demo1/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+
+  <router-view/>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+import Header from "@/components/Header.vue"
+
+export default {
+  name:"Layout",
+  components:{
+    Header
+  },
+}
+</script>
